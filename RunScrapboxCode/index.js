b5e7e903f72e8f682ebaaa690c5d31b1ab5521a4
Hideyoshi Moriya <hide@piyolab.com>
2018-08-13 18:12:28 +0900

    add RunScrapboxCode

---
+++ RunScrapboxCode/index.js
@@ -0,0 +1,31 @@
+function loadScript (url) {
+  return new Promise((resolve) => {
+    const script = document.createElement('script')
+    script.src = url
+    script.onload = resolve
+    document.body.appendChild(script)
+  })
+}
+
+async function loadAllScripts (urls) {
+  for (let url of urls) {
+    console.log(url)
+    await loadScript(url)
+  }
+}
+
+$(function(){
+  const args = {}
+  document.location.search.substring(1).split('&').forEach((s) => {
+    let [name, value] = s.split('=')
+    args[name] = decodeURIComponent(value)
+  })
+  const codelist = args['code']
+
+  console.log('codelist', codelist)
+
+  if (codelist) {
+    let urls = codelist.split(/,/)
+    loadAllScripts(urls)
+  }
+})
