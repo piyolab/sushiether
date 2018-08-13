5d03fc23cccb9d0a5d7522be6d7b82c073a6e911
Hideyoshi Moriya <hide@piyolab.com>
2018-08-13 18:59:42 +0900

    load web3

--- RunScrapboxCode/index.js
+++ RunScrapboxCode/index.js
@@ -14,12 +14,28 @@ async function loadAllScripts (urls) {
   }
 }
 
+function loadWeb3 (url) {
+  const script = document.createElement('script')
+  script.src = url
+  document.body.appendChild(script)
+}
+
 $(function(){
+
   const args = {}
   document.location.search.substring(1).split('&').forEach((s) => {
     let [name, value] = s.split('=')
     args[name] = decodeURIComponent(value)
   })
+
+  const web3Version = args['web3']
+  console.log('web3 version', web3Version)
+
+  if (web3Version) {
+    const web3Url = 'https://piyolab.github.io/sushiether/web3.js/libs/web3.js_v' + web3Version + '/web3.min.js'
+    loadWeb3(web3Url)
+  }
+
   const codelist = args['code']
 
   console.log('codelist', codelist)
